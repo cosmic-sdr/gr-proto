25b1a1a98c0172962434bd4b608b5a2f33c6a7cd
Seyong Lee <lees2@ornl.gov>
2019-01-11 15:27:59 -0500

    Add OpenARC-generated Aspen model for clLog.

--- gr-accenabled/lib/clLog_aspengen.bash
+++ gr-accenabled/lib/clLog_aspengen.bash
@@ -1,14 +1,5 @@
 #! /bin/bash
 
-if [ "$openarc" = "" ] || [ ! -d "${openarc}" ]; then
-    echo "Environment variable, openarc, should be set to the OpenARC home directory to use this script; exit"
-    exit
-fi
-
-if [ ! -f "${openarc}/bin/openarc" ]; then
-    echo "OpenARC executable, ${openarc}/bin/openarc, does not exist; run \"build.sh bin\" in the OpenARC root directory to build the executable; exit"
-    exit
-fi
 
 ########################################
 # OpenARC Option: ASPENModelGen        #
@@ -36,6 +27,53 @@ inputSource="${benchname}_kernel.c"
 entryFunction="${benchname}_kernel"
 runMode=3
 postprocessing=0
+
+function usage()
+{
+    echo "./${benchmark}_aspengen.bash"
+    echo "List of options:"
+    echo -e "\t-h --help"
+    echo -e "\t-p=name --program=name (default: ${benchname})"
+    echo -e "\t-i=data --input=data (default: ${inputSize1})"
+    echo -e "\t-f=inputfile --file=inputfile (default: ${inputSource})"
+    echo ""
+}
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -p | --program)
+            benchname=${VALUE}
+            ;;
+        -i | --input)
+            inputSize1=${VALUE}
+            ;;
+        -f | --file)
+            inputSource=${VALUE}
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ "$openarc" = "" ] || [ ! -d "${openarc}" ]; then
+    echo "Environment variable, openarc, should be set to the OpenARC home directory to use this script; exit"
+    exit
+fi
+
+if [ ! -f "${openarc}/bin/openarc" ]; then
+    echo "OpenARC executable, ${openarc}/bin/openarc, does not exist; run \"build.sh bin\" in the OpenARC root directory to build the executable; exit"
+    exit
+fi
 
 ${openarc}/bin/openarc -addIncludePath=${openarc}/openarcrt -SetAccEntryFunction=${entryFunction} -ASPENModelGen=mode=${runMode}:modelname=${benchname}:postprocessing=${postprocessing}:entryfunction=${entryFunction} -macro=GEN_ASPEN,__INPUTSIZE1__=${inputSize1} ${inputSource}
 
