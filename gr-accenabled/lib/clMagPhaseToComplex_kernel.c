944c75ba011ba0587a7bbf5ba642f9a88f05e6aa
Seyong Lee <lees2@ornl.gov>
2019-06-10 21:09:00 -0400

    Fix a bug in clMagPhaseToComplex_kernel.c

--- gr-accenabled/lib/clMagPhaseToComplex_kernel.c
+++ gr-accenabled/lib/clMagPhaseToComplex_kernel.c
@@ -3,11 +3,13 @@
 #include "math.h"
 
 void clMagPhaseToComplex_init(acc_device_t deviceType, int devSelector, int devId) {
-#if !defined(TR_MODE) || TR_MODE == 0
+#if !defined(TR_MODE) || TR_MODE == 0 || TR_MODE == 3
     acc_init(deviceType);
+#if !defined(TR_MODE) || TR_MODE == 0 
     if( devSelector == OCLDEVICESELECTOR_SPECIFIC ) { 
         acc_set_device_num(devId, deviceType);
     }   
+#endif
 #endif
 }
 
