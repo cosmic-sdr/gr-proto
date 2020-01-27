1df5446e3e8ea1046f141decbb02aa388b2731ee
Seyong Lee <lees2@ornl.gov>
2020-01-27 00:50:23 -0500

    Add more example in gr-openacc.

---
+++ gr-openacc/examples/example5/top_block.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Mon Jan 27 00:10:24 2020
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio.ctrlport.monitor import *
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import openacc
+import sip
+import sys
+from gnuradio import qtgui
+
+
+class top_block(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.qtgui_time_sink_x_1_0 = qtgui.time_sink_c(
+        	1024, #size
+        	samp_rate, #samp_rate
+        	"CPU Workflow", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_1_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_1_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_1_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_1_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_1_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_1_0.enable_grid(False)
+        self.qtgui_time_sink_x_1_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_1_0.enable_stem_plot(False)
+
+        if not True:
+          self.qtgui_time_sink_x_1_0.disable_legend()
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in xrange(2):
+            if len(labels[i]) == 0:
+                if(i % 2 == 0):
+                    self.qtgui_time_sink_x_1_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_1_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_1_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_1_0_win = sip.wrapinstance(self.qtgui_time_sink_x_1_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_1_0_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+        	1024, #size
+        	samp_rate, #samp_rate
+        	"OpenACC Workflow", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in xrange(2):
+            if len(labels[i]) == 0:
+                if(i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.openacc_accMagPhaseToComplex_1_0 = openacc.accMagPhaseToComplex(1, 0, 1, 0, 1)
+        self.openacc_accMagPhaseToComplex_1 = openacc.accMagPhaseToComplex(1, 0, 1, 0, 0)
+        self.openacc_accMagPhaseToComplex_0_0 = openacc.accMagPhaseToComplex(1, 0, 1, 0, 0)
+        self.openacc_accMagPhaseToComplex_0 = openacc.accMagPhaseToComplex(1, 0, 1, 0, 0)
+        self.openacc_accComplexToMag_0_0 = openacc.accComplexToMag(1, 0, 1, 0, 0)
+        self.openacc_accComplexToMag_0 = openacc.accComplexToMag(1, 0, 1, 0, 0)
+        self.openacc_accComplexToMagPhase_0_0 = openacc.accComplexToMagPhase(1, 0, 1, 0, 0)
+        self.openacc_accComplexToMagPhase_0 = openacc.accComplexToMagPhase(1, 0, 1, 1, 0)
+        self.openacc_accComplexToArg_0_0 = openacc.accComplexToArg(1, 0, 1, 0, 0)
+        self.openacc_accComplexToArg_0 = openacc.accComplexToArg(1, 0, 1, 0, 0)
+        self.blocks_magphase_to_complex_1_0 = blocks.magphase_to_complex(1)
+        self.blocks_magphase_to_complex_1 = blocks.magphase_to_complex(1)
+        self.blocks_magphase_to_complex_0_0 = blocks.magphase_to_complex(1)
+        self.blocks_magphase_to_complex_0 = blocks.magphase_to_complex(1)
+        self.blocks_ctrlport_monitor_performance_0 = not True or monitor("gr-perf-monitorx")
+        self.blocks_complex_to_magphase_0_0 = blocks.complex_to_magphase(1)
+        self.blocks_complex_to_magphase_0 = blocks.complex_to_magphase(1)
+        self.blocks_complex_to_mag_0_0 = blocks.complex_to_mag(1)
+        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
+        self.blocks_complex_to_arg_0_0 = blocks.complex_to_arg(1)
+        self.blocks_complex_to_arg_0 = blocks.complex_to_arg(1)
+        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.openacc_accComplexToMagPhase_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_complex_to_magphase_0, 0))
+        self.connect((self.blocks_complex_to_arg_0, 0), (self.blocks_magphase_to_complex_1, 1))
+        self.connect((self.blocks_complex_to_arg_0_0, 0), (self.blocks_magphase_to_complex_1_0, 1))
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_magphase_to_complex_1, 0))
+        self.connect((self.blocks_complex_to_mag_0_0, 0), (self.blocks_magphase_to_complex_1_0, 0))
+        self.connect((self.blocks_complex_to_magphase_0, 0), (self.blocks_magphase_to_complex_0, 0))
+        self.connect((self.blocks_complex_to_magphase_0, 1), (self.blocks_magphase_to_complex_0, 1))
+        self.connect((self.blocks_complex_to_magphase_0_0, 0), (self.blocks_magphase_to_complex_0_0, 0))
+        self.connect((self.blocks_complex_to_magphase_0_0, 1), (self.blocks_magphase_to_complex_0_0, 1))
+        self.connect((self.blocks_magphase_to_complex_0, 0), (self.blocks_complex_to_arg_0, 0))
+        self.connect((self.blocks_magphase_to_complex_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_magphase_to_complex_0_0, 0), (self.blocks_complex_to_arg_0_0, 0))
+        self.connect((self.blocks_magphase_to_complex_0_0, 0), (self.blocks_complex_to_mag_0_0, 0))
+        self.connect((self.blocks_magphase_to_complex_1, 0), (self.blocks_complex_to_magphase_0_0, 0))
+        self.connect((self.blocks_magphase_to_complex_1_0, 0), (self.qtgui_time_sink_x_1_0, 0))
+        self.connect((self.openacc_accComplexToArg_0, 0), (self.openacc_accMagPhaseToComplex_1, 1))
+        self.connect((self.openacc_accComplexToArg_0_0, 0), (self.openacc_accMagPhaseToComplex_1_0, 1))
+        self.connect((self.openacc_accComplexToMagPhase_0, 0), (self.openacc_accMagPhaseToComplex_0, 0))
+        self.connect((self.openacc_accComplexToMagPhase_0, 1), (self.openacc_accMagPhaseToComplex_0, 1))
+        self.connect((self.openacc_accComplexToMagPhase_0_0, 0), (self.openacc_accMagPhaseToComplex_0_0, 0))
+        self.connect((self.openacc_accComplexToMagPhase_0_0, 1), (self.openacc_accMagPhaseToComplex_0_0, 1))
+        self.connect((self.openacc_accComplexToMag_0, 0), (self.openacc_accMagPhaseToComplex_1, 0))
+        self.connect((self.openacc_accComplexToMag_0_0, 0), (self.openacc_accMagPhaseToComplex_1_0, 0))
+        self.connect((self.openacc_accMagPhaseToComplex_0, 0), (self.openacc_accComplexToArg_0, 0))
+        self.connect((self.openacc_accMagPhaseToComplex_0, 0), (self.openacc_accComplexToMag_0, 0))
+        self.connect((self.openacc_accMagPhaseToComplex_0_0, 0), (self.openacc_accComplexToArg_0_0, 0))
+        self.connect((self.openacc_accMagPhaseToComplex_0_0, 0), (self.openacc_accComplexToMag_0_0, 0))
+        self.connect((self.openacc_accMagPhaseToComplex_1, 0), (self.openacc_accComplexToMagPhase_0_0, 0))
+        self.connect((self.openacc_accMagPhaseToComplex_1_0, 0), (self.qtgui_time_sink_x_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_time_sink_x_1_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    if True:
+        if True:
+            (tb.blocks_ctrlport_monitor_performance_0).start()
+    else:
+        sys.stderr.write("Monitor '{0}' does not have an enable ('en') parameter.".format("tb.blocks_ctrlport_monitor_performance_0"))
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
